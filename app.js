51a4c326054ae251607fcb458fb0d782fdcef717
root <root@Fujitsu.ariana>
2018-08-08 01:59:50 +0800

    Updated and renamed app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var TeleBot=require("telebot"),
-    bot=new TeleBot("TOKEN"),
+    bot=new TeleBot(process.env.token),
     MongoClient=require('mongodb').MongoClient,
-    url='mongodb://localhost:27017';
+    url=process.env.url;
 MongoClient.connect(url,function(err,client){
-    if(err){return console.log(err);}
-    console.log("Connected to DB");
-    var db=client.db("db");
+	if(err){return console.log(err);}
+	console.log("Connected to DB");
+    var db=client.db("sophiegermainbotdb");
     var questions=db.collection("questions");
     questions.deleteMany({},function(err,res){if(err){return console.log(err);}});
-    var previd=0,topiclist=["Algebra","Calculus","Geometry & Topology","Number theory","Others","Physics"],groupid=-1001062964615,topictxt="";
-    for(let i=0;i<topiclist.length;i++){
-        topictxt+="   \n"+topiclist[i];
-    }
-    function sendtmpmsg(fmsg,msg){
+	var previd=0,topiclist=["Algebra","Calculus","Geometry & Topology","Number theory","Others","Physics"],groupid=-1001062964615,topictxt="";
+	for(let i=0;i<topiclist.length;i++){
+		topictxt+="   \n"+topiclist[i];
+	}
+	function sendtmpmsg(fmsg,msg){
         return bot.sendMessage(fmsg.chat.id,msg,{replyToMessage:fmsg.message_id,parseMode:"markdown",notification:false}).then(function(smsg){
             return setTimeout(function(err){
                 if(err){return console.log(err);}
@@ -20,36 +20,35 @@ MongoClient.connect(url,function(err,client){
             },10000);
         });
     }
-    bot.on("*",function(msg){
-        if(previd!=msg.chat.id){
-            console.log(msg.chat.id+" :");
-            previd=msg.chat.id;
-        }
-        console.log("   "+msg.from.first_name+"("+msg.from.id+") - "+msg.text);
-    });
-    bot.on("/start",function(msg){
-        let sentmsg=msg.reply.text("A bot for the [Telegram Mathematics group](t.me/mathsgroup)\nMade by [Ariana](tg://user?id=368439352)",{asReply:true,parseMode:"markdown"});
-        if(msg.chat.id==-1001062964615){
-            setTimeout(function(){
-                bot.deleteMessage(-1001062964615,sentmsg);
-            },10000);
-        }
-        return 0;
-    });
-    bot.on("/help",function(msg){
-        let sentmsg=bot.sendMessage(msg.chat.id,"List of commands:\n   /add _question-name_ _topic_\n   /list all or _topic_\n   /solve _question-name_ _topic_(must be either from admin or from OP)\n   /find _question-name_ _topic_\nList of topics:"+topictxt+"\nNote that analysis is under calculus and logic is vaugely under number theory, just to reduce number of topics",{replyToMessage:msg.message_id,parseMode:"markdown"});
-        if(msg.chat.id==-1001062964615){
-            return setTimeout(function(){
-                bot.deleteMessage(-1001062964615,sentmsg);
-            },10000);
-        }
-        return 0;
-    });
-    bot.on("/add",function(msg){
+	bot.on("*",function(msg){
+		if(previd!=msg.chat.id){
+			console.log(msg.chat.id+" :");
+			previd=msg.chat.id;
+		}
+		console.log("   "+msg.from.first_name+"("+msg.from.id+") - "+msg.text);
+	});
+	bot.on("/start",function(msg){
+		let sentmsg=msg.reply.text("A bot for the [Telegram Mathematics group](t.me/mathsgroup)\nMade by [Ariana](tg://user?id=368439352)",{asReply:true,parseMode:"markdown"});
+		if(msg.chat.id==-1001062964615){
+			setTimeout(function(){
+				bot.deleteMessage(-1001062964615,sentmsg);
+			},10000);
+		}
+		return 0;
+	});
+	bot.on("/help",function(msg){
+		let sentmsg=bot.sendMessage(msg.chat.id,"List of commands:\n   /add _question-name_ _topic_\n   /list all or _topic_\n   /solve _question-name_ _topic_(must be either from admin or from OP)\n   /find _question-name_ _topic_\nList of topics:"+topictxt+"\nNote that analysis is under calculus and logic is vaugely under number theory, just to reduce number of topics",{replyToMessage:msg.message_id,parseMode:"markdown"});
+		if(msg.chat.id==-1001062964615){
+			return setTimeout(function(){
+				bot.deleteMessage(-1001062964615,sentmsg);
+			},10000);
+		}
+		return 0;
+	});
+	bot.on("/add",function(msg){
         if(msg.chat.id!=groupid){return msg.reply.text("Please use the telegram math group",{asReply:true})}
         //format: reply to a question: /addqn question_name topic
-        let qnmsg=msg.reply_to_message;
-        if(!qnmsg){return sendtmpmsg(msg,"*Error*\nPlease reply the question with your message");}
+        if(!msg.reply_to_message){return sendtmpmsg(msg,"*Error*\nPlease reply the question with your message");}
         if(msg.text.split(" ").length<3){return sendtmpmsg(msg,"*Error*\nFormat: /add _question-name_ _topic_");}
         let qn=msg.text.split(/ (.+)/)[1];
         for(let i=0;i<topiclist.length+1;++i){
@@ -59,7 +58,7 @@ MongoClient.connect(url,function(err,client){
                 questions.find({"name":qnname,"topic":qntopic}).toArray(function(err,qn){
                     if(err){return console.log(err);}
                     if(qn[0]){return sendtmpmsg(msg,"*Error*\nA question with the same name and topic is still unanswered");}
-                    questions.insert([{"name":qnname,"topic":qntopic,"op":msg.from.id,"msgid":msg.message_id}],function(err,res){
+                    questions.insert([{"name":qnname,"topic":qntopic,"op":msg.from.id,"msgid":msg.reply_to_message.message_id}],function(err,res){
                         if(err){return console.log(err);}
                         return msg.reply.text("Added question",{asReply:true});
                     });
